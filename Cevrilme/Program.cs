bf2d3709a5eacf10c9eb6eadb0b12d1729007b99
Resad Memmedov <talehg2932@gmail.com>
2021-04-24 21:52:02 +0400

    2 yeni meselenin hell olunmasi

---
+++ Cevrilme/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Cevrilme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            /*
+             * Çevrilmə
+            Hər hansı bir natural n ədədini götürək. Onu növbəti şəkildə dəyişdirəcəyik: Əgər ədəd cütdürsə, onda onu 2-ə bölək, əgər təkdirsə ona 1 əlavə edək. Bir neçə belə dəyişmədən sonra həmişə 1 alacağıq. Məsələn, 11 ədədindən 12 ədədi alınır, sonra 6, 3, 4, 2 və sonda 1. Beləliklə, 11-dən 1 almaq üçün 6 dəyişiklik etmək lazımdır.
+
+            Verilmiş natural ədədə görə 1 alınana qədər onun dəyişmələrinin sayını tapın.
+
+            Giriş verilənləri
+            Natural n (1 ≤ n ≤ 109) ədədi.
+
+            Çıxış verilənləri
+            1 alınana qədər n ədədinin dəyişmələrinin sayını çap edin.
+
+            Giriş verilənləri #1 content_copy
+            11
+            Çıxış verilənləri #1 content_copy
+            6
+             */
+            #endregion
+            #region
+            int eded = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (eded >=2)
+            {
+                if (eded % 2 == 0)
+                {
+                    eded = eded / 2;
+                    i++;
+                }
+                else if (eded % 2 == 1)
+                {
+                    eded = eded + 1;
+                    i++;
+                }
+            }
+            Console.WriteLine(i);
+            #endregion
+        }
+
+    }
+}
---
+++ Deniz quldurlari ve sikkeler/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Deniz_quldurlari_ve_sikkeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region
+            /*
+             * Dəniz quldurları və sikkələr
+            n dəniz qulduruna m qızıl sikkədən ibarət xəzinəni öz aralarında ədalətli şəkildə bölmək müyəssər oldu – hər kəs öz quldur ranqına (dərəcəsinə) və stajına uyğun ona çatacaq payı aldı. Ən kiçik quldur a sikkə, hər sonrakı quldur özündən əvvəlki yoldaşından bir sikkə artıq götürdü. Axırda kapitan idi, ona planlaşdırıldığından iki dəfə artıq sikkə çatdı və aydındır ki, ondan sonra sikkə qalmadı.
+
+            Əgər a və m məlumdursa, kapitanla birlikdə neçə quldur var idi. Komandasız kapitan adi quldurdur, onda n > 1.
+
+            Giriş verilənləri
+            İki natural a və m (1 ≤ a ≤ 100, m < 15150) ədədləri. Bütün giriş verilənləri düzgündür.
+
+            Çıxış verilənləri
+            Quldurların n sayı.
+
+            Giriş verilənləri #1 content_copy
+            5 25
+            Çıxış verilənləri #1 content_copy
+            3
+             */
+            #endregion
+            #region
+            string valu = Console.ReadLine();
+            var Array = valu.Split();
+            int a = Convert.ToInt32(Array[0]);
+            int m = Convert.ToInt32(Array[1]);
+            int i = 1;
+            x:
+            if ((m - a) / 2 >= a)
+            {
+                i++;
+                m = m - a;
+                a++;
+                goto x;
+            }
+            Console.WriteLine(i);
+            #endregion
+        }
+    }
+}
